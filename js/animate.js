c7c450afed2755203aa10de8895cdb76c46d9d6e
SEIMIN10 <inerkq12@gmail.com>
2022-12-14 17:56:35 +0900

    .

--- js/animate.js
+++ js/animate.js
@@ -2,13 +2,12 @@ const scroll = document.querySelector(".box1");
 
 window.addEventListener("wheel", (e) => {
   console.log(e.deltaY, e.deltaX);
-
 });
 
 const options = {
-  root: null, 
+  root: null,
   rootMargin: "0px",
-  threshold: 0.5, 
+  threshold: 0.5,
 };
 
 const observer = new IntersectionObserver((entries) => {
@@ -26,8 +25,16 @@ const titleList = document.querySelectorAll(".box1");
 const titleList1 = document.querySelectorAll(".box2");
 const titleList2 = document.querySelectorAll(".box3-1");
 const titleList3 = document.querySelectorAll(".box3");
+const titleList4 = document.querySelectorAll(".box4");
+const titleList5 = document.querySelectorAll(".box4-1");
+const titleList6 = document.querySelectorAll(".box5");
+const titleList7 = document.querySelectorAll(".box5-1");
 
 titleList.forEach((el) => observer.observe(el));
 titleList1.forEach((el) => observer.observe(el));
 titleList2.forEach((el) => observer.observe(el));
 titleList3.forEach((el) => observer.observe(el));
+titleList4.forEach((el) => observer.observe(el));
+titleList5.forEach((el) => observer.observe(el));
+titleList6.forEach((el) => observer.observe(el));
+titleList7.forEach((el) => observer.observe(el));
