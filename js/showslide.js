ced59e055ab4c92994c4c099950ca61b44457067
SEIMIN123 <interkp12@gmail.com>
2022-12-17 15:56:21 +0900

    .

--- js/showslide.js
+++ js/showslide.js
@@ -19,3 +19,27 @@ function showSlides() {
   dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 4000);
 }
+
+// -----------------------------------------------------------
+
+let slideIndex1 = 0;
+showSlides1();
+
+function showSlides1() {
+  let i;
+  let slides1 = document.getElementsByClassName("mySlides1");
+  let dots1 = document.getElementsByClassName("dot1");
+  for (i = 0; i < slides1.length; i++) {
+    slides1[i].style.display = "none";
+  }
+  slideIndex1++;
+  if (slideIndex1 > slides1.length) {
+    slideIndex1 = 1;
+  }
+  for (i = 0; i < dots1.length; i++) {
+    dots1[i].className = dots1[i].className.replace(" active", "");
+  }
+  slides1[slideIndex1 - 1].style.display = "block";
+  dots1[slideIndex1 - 1].className += " active";
+  setTimeout(showSlides1, 4000);
+}
